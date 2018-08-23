c422d0881f76cace5a9d20746facacaacb41dc60
serigne007mor <serignemortoure1@gmail.com>
2018-08-23 10:29:04 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Baily McFadden, Serigne Toure 
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Serigne Toure, Computer engineering\n");
+
+  return 0;
+}
+
